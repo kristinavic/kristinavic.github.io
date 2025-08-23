a2797811c4109a543292afdb3daf3d4171979310
KristinaV <147422872+kristinavic@users.noreply.github.com>
2025-08-23 20:32:57 +0200

    Added JS script fo toggle switch effect

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+class CssPropControl {
+    constructor(element) {
+        this.element = element;
+    }
+
+    get(varName) {
+        return getComputedStyle(this.element).getPropertyValue(varName);
+        /*gets selected element and get property val. assosiated with varName */
+    }
+
+    set(varName, value) {
+        return this.element.style.setProperty(varName, value);
+        /* assign varName to given value) */
+    }
+}
+
+const bodyCssProps = new CssPropControl(document.body);
+
 let toggle = document.querySelector('#dark-mode-toggle')
 toggle.addEventListener('click', () => {
-    if (toggle.checked) {
-        document.body.style.setProperty('background', '#16263B')
-        document.body.style.setProperty('color', 'white')
-    } 
-    else {
-        document.body.style.setProperty('background', '#ECF5F8')
-        document.body.style.setProperty('color', '#16263B')
-    }
+    let mode = toggle.checked ? 'dark' : 'light' /* get current mode depending on toggle state */
+    bodyCssProps.set('--background-color', bodyCssProps.get(`--${mode}-background-color`)) 
+    bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`)) 
+    // bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))   
+    /* set background color depending on mode */
 
 })
